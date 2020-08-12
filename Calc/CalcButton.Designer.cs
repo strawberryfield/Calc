f14d50a3809e8e11947f8a7533f3aea2b063f3eb
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2020-08-12 21:32:25 +0200

    CalcButton

---
+++ Calc/CalcButton.Designer.cs
@@ -0,0 +1,93 @@
+﻿// copyright (c) 2020 Roberto Ceccarelli - CasaSoft
+// http://strawberryfield.altervista.org 
+// 
+// This file is part of CasaSoft Calc
+// 
+// CasaSoft Calc is free software: 
+// you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// CasaSoft Calc is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+// See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with CasaSoft Calc.  
+// If not, see <http://www.gnu.org/licenses/>.
+
+namespace Casasoft.Calc
+{
+    partial class CalcButton
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.Location = new System.Drawing.Point(2, 2);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(45, 12);
+            this.label.TabIndex = 64;
+            this.label.Text = "Label";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.SaddleBrown;
+            this.button.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button.ForeColor = System.Drawing.Color.White;
+            this.button.Location = new System.Drawing.Point(3, 13);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(45, 23);
+            this.button.TabIndex = 63;
+            this.button.Text = "BTN";
+            this.button.UseVisualStyleBackColor = false;
+            // 
+            // CalcButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button);
+            this.Name = "CalcButton";
+            this.Size = new System.Drawing.Size(48, 36);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button button;
+    }
+}
